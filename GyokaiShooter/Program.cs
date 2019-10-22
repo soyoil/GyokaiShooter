72075a1645a75d21d5d745656e049dd239521682
soyoil <kappamkai8801@gmail.com>
2019-10-22 22:14:34 +0900

    初期設定

--- GyokaiShooter/Program.cs
+++ GyokaiShooter/Program.cs
@@ -8,8 +8,21 @@ namespace GyokaiShooter
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            // Altseedを初期化する。
+            asd.Engine.Initialize("Empty", 640, 480, new asd.EngineOption());
+
+            // Altseedが進行可能かチェックする。
+            while (asd.Engine.DoEvents())
+            {
+                // Altseedを更新する。
+                asd.Engine.Update();
+            }
+
+            // Altseedを終了する。
+            asd.Engine.Terminate();
         }
     }
 }
