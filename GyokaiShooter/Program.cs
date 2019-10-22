64a76f9cd61b3964155c657dbefdd3f0e016d6f3
soyoil <kappamkai8801@gmail.com>
2019-10-22 22:55:53 +0900

    タイトル画面追加

--- GyokaiShooter/Program.cs
+++ GyokaiShooter/Program.cs
@@ -11,8 +11,18 @@ namespace GyokaiShooter
         [STAThread]
         static void Main(string[] args)
         {
+            // ウィンドウサイズ可変，リソース再読み込み有効
+            asd.EngineOption engineOption = new asd.EngineOption
+            {
+                IsWindowResizable = true,
+                IsReloadingEnabled = true
+            };
+
             // Altseedを初期化する。
-            asd.Engine.Initialize("Empty", 640, 480, new asd.EngineOption());
+            asd.Engine.Initialize("Empty", 640, 480, engineOption);
+
+            var title = new Title();
+            asd.Engine.ChangeScene(title);
 
             // Altseedが進行可能かチェックする。
             while (asd.Engine.DoEvents())
---
+++ GyokaiShooter/Title.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GyokaiShooter
+{
+    class Title : asd.Scene
+    {
+        protected override void OnRegistered()
+        {
+            var backGround = new TitleBackground();
+            AddLayer(backGround);
+        }
+
+    }
+
+    class TitleBackground : asd.Layer2D
+    {
+        protected override void OnAdded()
+        {
+            var font = asd.Engine.Graphics.CreateDynamicFont("", 32, new asd.Color(255, 255, 255, 255), 1, new asd.Color(255, 255, 255, 255));
+            var obj = new asd.TextObject2D
+            {
+                Font = font,
+                Position = new asd.Vector2DF(0, 0),
+                Text = "title"
+            };
+            AddObject(obj);
+        }
+    }
+}
